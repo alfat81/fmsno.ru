2da8b02017c889440576b27d8d168ee198813471
alfat81 <52703823+alfat81@users.noreply.github.com>
2025-11-28 20:30:46 +0300

    Create .env.js

---
+++ .env.js
@@ -0,0 +1,5 @@
+// .env.js - Переменные окружения (НЕ КОМИТЬТЕ В GITHUB!)
+window.ENV = {
+    TELEGRAM_BOT_TOKEN: '8461977678:AAHXGOs2mSyHsAEXsHHUro58j638iIHwm6U', // Ваш реальный токен
+    TELEGRAM_CHAT_ID: '-1003455512571'    // ID вашего канала
+};
